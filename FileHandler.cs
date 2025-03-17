0d0c85f647c9420e573b6a704740441837598116
RysichIvanna <zipz231_rio@student.ztu.edu.ua>
2025-03-17 21:19:24 +0200

    Refactor: Split MainWindow class to resolve Bloaters issue

---
+++ FileHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using Microsoft.Win32;
+
+namespace lab
+{
+    public class FileHandler
+    {
+        public static void OpenFile(RichTextBox richTextBox)
+        {
+            OpenFileDialog dlg = new OpenFileDialog { Filter = "Document files (*.doc)|*.doc" };
+            var result = dlg.ShowDialog();
+            if (result == true)
+            {
+                TextRange textRange = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+                SaveLoad.LoadDocumentFromFile(dlg.FileName, textRange);
+            }
+        }
+
+        public static void SaveFile(RichTextBox richTextBox, Window window, ref bool isSaved)
+        {
+            SaveFileDialog savefile = new SaveFileDialog { FileName = DateTime.Now.ToString("yyyyMMdd_HHmmss"), Filter = "Document files (*.doc)|*.doc" };
+            if (savefile.ShowDialog() == true)
+            {
+                TextRange textRange = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+                var result = SaveLoad.SaveDocumentToFile(savefile.FileName, textRange);
+                window.Title = Utilily.GetFileNameFromPath(result.fileName);
+                isSaved = result.isSaved;
+            }
+        }
+    }
+}
